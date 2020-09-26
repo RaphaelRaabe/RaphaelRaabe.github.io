04e042e86af52cd4b25ee28ec35cd855f2846f35
Amir H. E <piano337@yahoo.de>
2020-09-26 16:54:22 +0200

    Found a solution for thumbnails (with overhead)

---
+++ prepScripts/paintingsCSVGenerator.sh
@@ -0,0 +1,23 @@
+dir_data="_data"
+dir_repo="RaphaelRaabe.github.io"
+dir_cur="$PWD"
+
+[ "$dir_cur" == *"$dir_repo"* ] && echo "Go to '$dir_repo' repository dir!" && exit
+
+while [[ "${PWD##*/}" != "$dir_repo" ]]; do
+  cd ..
+done
+
+[ "${PWD##*/}" == "$dir_repo" ] && cd $dir_data
+
+echo "name,orig,thumb" > paintings.csv
+
+for f in ../img/paintings/*; do
+  raw=$(readlink -f "$f")
+  filename="${raw##*/}"
+
+  name="${filename%.*}"
+  orig="${raw#*"$dir_repo"}"
+  thumb="/img/thumb/thumb.${name%-*}.jpg"
+  echo "$name,$orig,$thumb" >> paintings.csv
+done
